a46d4afadd8c54d7bc81f6b5e343087cde97b75a
Sanekli <153511859+Sanekli@users.noreply.github.com>
2024-06-28 16:53:45 +0100

    Update Product.js

--- src/Components/ourHome/Product.js
+++ src/Components/ourHome/Product.js
@@ -19,7 +19,7 @@ let userIsAdmin = JSON.parse(localStorage.getItem('current_user'))
   return (
     
 <div >
-    {userIsAdmin?.role==='admin' ? <Admin userId={product._id}/> : ''}
+{userIsAdmin?.role==='admin' ? <Admin userId={product._id}/> : ''}
 <section className="dark" >
   {productList?.map((product) => 
   <div  className="container py-4" key={product._id}>
@@ -49,8 +49,6 @@ let userIsAdmin = JSON.parse(localStorage.getItem('current_user'))
         </div>
       </article>
       </Link>
-      
-      
       </div>
   )}
   </section>
